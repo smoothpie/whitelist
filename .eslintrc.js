813feb266e6d9a2dc833d0102a8d5dba0f86a586
Sergei Nepryakhin <sergey.nepryahin@dashboquet.com>
2020-04-19 10:27:45 +0300

    feat: refresh eslint prettier typescript config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,44 @@
+// basic .eslintrc.js compatible with react prettier and typescript
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  // Specifies the ESLint parser for TypeScript
+  parser: "@typescript-eslint/parser",
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: "detect",
+    },
   },
   env: {
     browser: true,
     node: true,
-    es6: true
+    es6: true,
   },
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ["@typescript-eslint", "react", "prettier"],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module' // Allows for the use of imports
+    // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2018,
+    // Allows for the use of imports
+    sourceType: "module",
   },
   rules: {
-    'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    // Disable prop-types as we use TypeScript for type checking
+    "react/prop-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    // Enable prettier rules
+    "prettier/prettier": "error",
+    // interface start with capital I
+    "@typescript-eslint/interface-name-prefix": "off",
+    // allow "any" as type
+    "@typescript-eslint/no-explicit-any": "off",
+    // allow @ts-ignore for testing purposes
+    "@typescript-eslint/ban-ts-ignore": "off",
   },
-  overrides: [
-    // Override some TypeScript rules just for .js files
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off' //
-      }
-    }
-  ]
 };
