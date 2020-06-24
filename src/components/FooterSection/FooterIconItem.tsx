ba69fbb36440fb9a517d5be910c6cf85586a35a5
dariavv <daria.sidorovich333@gmail.com>
2020-06-24 13:40:34 +0300

    refactor: refactoring animation

--- src/components/FooterSection/FooterIconItem.tsx
+++ src/components/FooterSection/FooterIconItem.tsx
@@ -7,22 +7,31 @@ type Props = {
 };
 
 const FooterIconItem: React.FC<Props> = ({ link, children }) => {
-  const [isHovered, setHovered] = useState(false);
+  // TODO: ask questions
+  // const [isHovered, setHovered] = useState(false);
 
-  const props = useSpring({
-    transform: isHovered
-      ? "scale(1.2) rotate(360deg)"
-      : "scale(1) rotate(0deg)",
+  // const props = useSpring({
+  //   transform: isHovered
+  //     ? "scale(1.2) rotate(360deg)"
+  //     : "scale(1) rotate(0deg)",
+  //   config: config.slow
+  // });
+
+  const [props, set] = useSpring(() => ({
+    transform: "scale(1) rotate(0deg)",
     config: config.slow
-  });
+  }));
 
   return (
     <FooterIcon
       href={link}
       target="blank_"
       style={props}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
+      // onMouseEnter={() => setHovered(true)}
+      // onMouseLeave={() => setHovered(false)}
+
+      onMouseEnter={() => set({ transform: "scale(1.2) rotate(360deg)" })}
+      onMouseLeave={() => set({ transform: "scale(1) rotate(0deg)" })}
     >
       {children}
     </FooterIcon>
