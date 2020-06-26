4f070776c103bdda7f15cc80a3dd3c511d64a7b6
dariavv <daria.sidorovich333@gmail.com>
2020-06-27 00:00:42 +0300

    refactor: refactoring Reviews component

---
+++ src/components/Reviews/ReviewsSliderItem.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Button from "../Button";
+import { SlideImage, Review, Description } from "./styled";
+import { H4, SliderItem, InnerBlock, SliderText } from "../Typography";
+
+type Props = {
+  fluid: any;
+  review: string;
+  person: string;
+  desc: string;
+};
+
+const ReviewsSliderItem: React.FC<Props> = ({
+  fluid,
+  review,
+  person,
+  desc
+}) => {
+  return (
+    <SliderItem>
+      <SlideImage fluid={fluid} />
+      <InnerBlock>
+        <SliderText>
+          <Review>{review}</Review>
+          <H4>{person}</H4>
+          <Description>{desc}</Description>
+          <Button
+            link="https://n247635.yclients.com/company:242564?o=m704631"
+            dataBack="Оставить отзыв"
+            dataFront="Оставить отзыв"
+          />
+        </SliderText>
+      </InnerBlock>
+    </SliderItem>
+  );
+};
+
+export default ReviewsSliderItem;
