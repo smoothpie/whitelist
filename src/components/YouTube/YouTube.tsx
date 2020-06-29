f623faf245ce1b29a6a0c3d06bdfd148298b0377
dariavv <daria.sidorovich333@gmail.com>
2020-06-29 14:16:24 +0300

    feat: add animation to YouTube button

--- src/components/YouTube/YouTube.tsx
+++ src/components/YouTube/YouTube.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { YouTubeSection, YouTubeButton } from "./styled";
 import { SectionTitle, SectionDescription, StyledLinkA } from "../Typography";
+import { useTransition, animated } from "react-spring";
 
 const YouTube: React.FC = () => {
+  const [isVisible, setVisible] = useState<boolean | undefined>(false);
   const {
     youtube: { frontmatter }
   } = useStaticQuery(graphql`
@@ -18,13 +20,43 @@ const YouTube: React.FC = () => {
   `);
 
   const { title, description } = frontmatter;
+
+  const message = useTransition(isVisible, null, {
+    from: { opacity: 0, transform: "translateY(-4rem)" },
+    enter: { opacity: 1, transform: "translateY(0rem)" },
+    leave: { opacity: 0, transform: "translateY(-4rem)" },
+    config: { duration: 2000 }
+  });
+
+  const handleClick = () => {
+    if (isVisible) {
+      const a = document.createElement("a");
+      a.href = "https://www.youtube.com/";
+      a.target = "blank_";
+      a.rel = "noopener noreferrer";
+      a.click();
+      setVisible(false);
+    }
+    if (!isVisible) {
+      setVisible(true);
+    }
+  };
+
   return (
     <YouTubeSection>
       <SectionTitle>{title}</SectionTitle>
       <SectionDescription>{description}</SectionDescription>
-      <StyledLinkA href="/#" target="blank_" rel="noopener noreferrer">
-        <YouTubeButton />
+      <StyledLinkA as="div">
+        <YouTubeButton onClick={handleClick} />
       </StyledLinkA>
+      {message.map(
+        ({ item, key, props }: any) =>
+          item && (
+            <animated.h2 style={props} key={key}>
+              Coming soon...
+            </animated.h2>
+          )
+      )}
     </YouTubeSection>
   );
 };
