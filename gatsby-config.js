589096fd574a51f130c87e1874a4fa97b605bc8f
dariavv <daria.sidorovich333@gmail.com>
2020-07-06 10:15:25 +0300

    fix: fix gatsby-config.js and fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-offline",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-typescript",
@@ -76,11 +75,56 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
+        name: "911barber",
+        short_name: "911barber",
         start_url: "/",
+        display: `standalone`,
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
         display: "minimal-ui",
-        icon: "src/assets/images/favicon.jpg" // This path is relative to the root of the site.
+        icon: `src/assets/images/favicon.jpg`, // This path is relative to the root of the site.
+        icons: [
+          {
+            src: "/icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
       }
     },
     {
@@ -94,6 +138,7 @@ module.exports = {
           failOnError: false
         }
       }
-    }
+    },
+    "gatsby-plugin-offline"
   ]
 };
